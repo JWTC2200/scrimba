b2a30c9eaea6d38c201ee0f2c15f48c165b7bbb5
JWTC2200 <113539489+JWTC2200@users.noreply.github.com>
2023-07-06 14:07:11 +0100

    basic grid

--- module6/starrail/src/App.jsx
+++ module6/starrail/src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header"
+import Main from "./components/Main"
 
 
 export default function App() {
@@ -6,6 +7,7 @@ export default function App() {
   return (
     <div>
       <Header />
+      <Main />
     </div>
       
   )
---
+++ module6/starrail/src/components/Main.jsx
@@ -0,0 +1,37 @@
+import {charData} from "../data/chardata.js"
+
+export default function Main() {
+
+    const postElements = charData.map(entry => {
+        const splashPath = `./src/images/splash_${entry.fileID}.png`
+        const quotePath = `./src/images/quote_${entry.fileID}.png`
+        const typePath = `./src/images/icon_${entry.type}.png`
+        const storyElements = entry.story.map(line => <p>{line}</p>)
+
+        return (
+            <div 
+                key={entry.id} 
+                id={entry.id} 
+                className="character-post"
+            >
+                <div className="post-container">
+                    <img className="post-splash" src={splashPath}></img>
+                    <div className="post-title">
+                        <h1>{entry.name}</h1>
+                        <img className="type-icon" src={typePath}></img>
+                    </div>
+                    {storyElements}
+                    <img className="post-quote" src={quotePath}></img>
+                </div>
+                
+            </div>
+        )
+    })
+
+    return(
+        <main>
+            {/* <img src="./src/images/bg_starrail.png"></img> */}
+            {postElements}
+        </main>
+    )
+}
