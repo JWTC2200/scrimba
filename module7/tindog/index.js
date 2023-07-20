90965816f2eb1d51f3bc30e29966c5fc94605490
JWTC2200 <113539489+JWTC2200@users.noreply.github.com>
2023-07-20 15:57:27 +0100

    tindog css done

--- module7/tindog/index.js
+++ module7/tindog/index.js
@@ -1,1 +1,8 @@
-// Remember to import the data and Dog class!
+import dogs from "./data.js";
+import { Dog } from "./util.js";
+
+
+const dogsArray = dogs
+const test = new Dog(dogsArray[0])
+
+console.log(test)
