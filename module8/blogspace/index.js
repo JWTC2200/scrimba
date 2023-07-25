2819840bd73f131b0ec65518b90b6e6a9858dcf8
JWTC2200 <113539489+JWTC2200@users.noreply.github.com>
2023-07-25 10:30:04 +0100

    colorscheme

--- module8/blogspace/index.js
+++ module8/blogspace/index.js
@@ -1,32 +1,49 @@
-/**
- Challenge:
- 
- GET a list of blog posts from the JSON Placeholder API.
- 
- BaseURL: https://apis.scrimba.com/jsonplaceholder/
- Endpoint: /posts
- 
- Since there's so many posts, let's limit the array to just 5 items.
- You can use the `.slice()` array method to just grab the first 5 objects
- from the data array that comes back from the API
- 
- Log the 5 items to the console
- */
-
 const postContainer = document.getElementById("post-container")
+const newTitle = document.getElementById("post-title")
+const newText = document.getElementById("post-text")
+const form = document.getElementById("new-post")
+let postsArray = []
+
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(response => response.json())
     .then(data => {
-        console.log(data.slice(0,1))
-        const postsArray = data.slice(0, 5)
-        const postsHTML = postsArray.map(post =>
-            `
-            <div class="post-item">
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            </div>`
-        )
-        postsHTML.forEach(post => postContainer.innerHTML += post)
+        postsArray = data.slice(0, 5)
+        addPostHtml(postsArray)
     })
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    
+    const newPost = {
+        title: newTitle.value,
+        body: newText.value
+    }
+
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
+        method: "POST",
+        body: JSON.stringify(newPost),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(res => res.json())
+    .then(data =>  {
+        postsArray.unshift(data)
+        addPostHtml(postsArray)
+        form.reset()
+    })})
+
+function addPostHtml(array) {
+    postContainer.innerHTML = ""
+    const postsHTML = array.map(post =>
+        `
+        <div class="post-item">
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+        </div>`
+    )
+    postsHTML.forEach(post => postContainer.innerHTML += post)
+}
