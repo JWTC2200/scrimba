346ed22e29ad02c86da8b879c25b0667b7e91601
JWTC2200 <113539489+JWTC2200@users.noreply.github.com>
2023-07-27 10:38:21 +0100

    async test

--- module9/deck/index.js
+++ module9/deck/index.js
@@ -5,32 +5,25 @@ let deckId
 const drawButton = document.getElementById("draw-cards")
 drawButton.disabled = true
 
-function handleClick() {
-    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-        .then(res => res.json())
-        .then(data => {
-            updateRemainingCards(data)
-            deckId = data.deck_id
-            drawButton.disabled = false
-        })
+async function handleClick() {
+    const response = await fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    const data = await response.json()
+    updateRemainingCards(data)
+    deckId = data.deck_id
+    drawButton.disabled = false
+}
+
+async function drawCards() {
+    const response = await fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    const data = await response.json()
+    updateRemainingCards(data)
+    renderCards(data.cards)
+    compareCards(data.cards)
+    if (document.getElementById("cards-remaining").textContent === "0") {
+        drawButton.disabled = true
+        finalWinner()
+    }   
 }
 
 document.getElementById("new-deck").addEventListener("click", handleClick)
-
-drawButton.addEventListener("click", () => {   
-    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
-        .then(res => res.json())
-        .then(data => {
-            updateRemainingCards(data)
-            renderCards(data.cards)
-            compareCards(data.cards)
-            if (document.getElementById("cards-remaining").textContent === "0") {
-                drawButton.disabled = true
-                finalWinner()
-            }   
-        })
-     
-})
-
-
-
+drawButton.addEventListener("click", drawCards)
