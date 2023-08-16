3b5bf6ea70ed0cdbe10a47e60c60efbc5d4e61b8
JWTC2200 <113539489+JWTC2200@users.noreply.github.com>
2023-08-16 17:29:58 +0100

    no navbar test

--- module12/vanlife/src/App.jsx
+++ module12/vanlife/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Navbar from "./pages/Nav/Navbar"
+// import Navbar from "./pages/Nav/Navbar"
 import About from "./pages/About/About"
 import Home from "./pages/Home/Home"
 import Footer from "./pages/Footer/Footer"
@@ -12,7 +12,7 @@ export default function App() {
 
   return (
     <BrowserRouter>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>} />
