812066ec152d57c2f249de8486949e24846e4c61
JWTC2200 <113539489+JWTC2200@users.noreply.github.com>
2023-09-08 16:10:01 +0100

    reviews done

--- module12/vanlife/src/pages/Host/Income.jsx
+++ module12/vanlife/src/pages/Host/Income.jsx
@@ -1,5 +1,33 @@
+import incomegraph from "../../assets/images/income-graph.png"
+
 export default function Income() {
     return (
-        <h1>INCOME</h1>
+        <div className="host-income-container">
+            <h2 className="bold mp0 fs24">Income</h2>
+            <p> Last <span className="underline bold">30 days</span></p>
+            <h3 className="bold mp0 fs32">$2,260</h3>
+            <img className="host-graph" src={incomegraph}/>
+            <div>
+                <div className="flex-between">
+                    <h4 className="bold mp0">Your transactions (3)</h4>
+                    <p>Last <span className="fs14 underline bold">30 days</span></p>
+                </div>
+                <div className="host-income-container">
+                    <div className="host-income-transaction flex-between">
+                        <div className="mp0 fs24 bold">$720</div>
+                        <div className="mp0 fs14">1/12/22</div>
+                    </div>
+                    <div className="host-income-transaction flex-between">
+                        <div className="mp0 fs24 bold">$560</div>
+                        <div className="mp0 fs14">10/11/22</div>
+                    </div>
+                    <div className="host-income-transaction flex-between">
+                        <div className="mp0 fs24 bold">$980</div>
+                        <div className="mp0 fs14">23/11/22</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     )
 }
--- module12/vanlife/src/pages/Host/Reviews.jsx
+++ module12/vanlife/src/pages/Host/Reviews.jsx
@@ -1,5 +1,10 @@
+import reviewgraph from "../../assets/images/reviews-graph.png"
+
 export default function Reviews() {
     return (
-        <h1>REVIEWS</h1>
+        <div className="host-reviews-container">
+            <h2 className="bold mp0 fs24">Your reviews  <span className="fs16 fw500">last <span className="underline bold">30 days</span></span></h2>
+            <img className="host-graph mt4" src={reviewgraph}/>
+        </div>
     )
 }
